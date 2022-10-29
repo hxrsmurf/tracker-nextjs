5023fe2309ef757f051a10fadb463c96353153f3
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-29 16:52:24 -0400

    Add useRouter

--- Components/Navigation.js
+++ Components/Navigation.js
@@ -1,17 +1,23 @@
 import { Grid } from "@mui/material";
 import Button from "@mui/material/Button";
+import { useRouter } from "next/router";
 import LoginButton from "./LoginButton";
 
 export default function Navigation() {
+  const router = useRouter();
 
   return (
     <div>
       <Grid container justifyContent="space-between">
         <Grid item>
-          <Button variant="contained"> Dashboard </Button>
+          <Button variant="contained" onClick={() => router.push("/")}>
+            Dashboard
+          </Button>
         </Grid>
         <Grid item>
-          <Button variant="contained"> Movies </Button>
+          <Button variant="contained" onClick={() => router.push("/movies")}>
+            Movies
+          </Button>
         </Grid>
         <Grid item>
           <LoginButton />
