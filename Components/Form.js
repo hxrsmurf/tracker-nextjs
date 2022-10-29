4a9e6cb8f2229cecb6539822ebc622a6cd09134f
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-29 19:33:29 -0400

    Initial form

---
+++ Components/Form.js
@@ -0,0 +1,7 @@
+export default function Form({ type }) {
+  return (
+    <>
+        Welcome to the form for {type}
+    </>
+  )
+}
--- pages/movies.js
+++ pages/movies.js
@@ -1,8 +1,17 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import Form from "../Components/Form";
 
 export default function movies() {
   const { data: session } = useSession();
   if (!session) useRouter().push("/");
-  return <div>movies</div>;
+  return (
+    <>
+      <div>
+        <Form type="movie"/>
+      </div>
+
+      <div>Movies</div>
+    </>
+  );
 }
