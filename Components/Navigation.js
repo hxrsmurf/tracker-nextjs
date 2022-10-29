f12ed302a00c712cde750ff4fe7fe21651f227eb
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-29 16:48:54 -0400

    Add justifyContent to navigation

--- Components/Navigation.js
+++ Components/Navigation.js
@@ -1,15 +1,17 @@
 import { Grid } from "@mui/material";
 import Button from "@mui/material/Button";
-import { useState } from "react";
 import LoginButton from "./LoginButton";
 
 export default function Navigation() {
 
   return (
     <div>
-      <Grid container spacing={2}>
-        <Grid item sm={10}>
+      <Grid container justifyContent="space-between">
+        <Grid item>
           <Button variant="contained"> Dashboard </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="contained"> Movies </Button>
         </Grid>
         <Grid item>
           <LoginButton />
