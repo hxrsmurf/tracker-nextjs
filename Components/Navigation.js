9d1d611ce51197514c3c779c47199c4f20e746f4
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-29 16:42:02 -0400

    Basic navigation

--- Components/Navigation.js
+++ Components/Navigation.js
@@ -1,51 +1,18 @@
 import { Grid } from "@mui/material";
 import Button from "@mui/material/Button";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
 import { useState } from "react";
 import LoginButton from "./LoginButton";
 
 export default function Navigation() {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
   return (
     <div>
       <Grid container spacing={2}>
         <Grid item sm={10}>
-          <Button
-            id="basic-button"
-            aria-controls={open ? "basic-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            onClick={handleClick}
-            style={{ color: "white", fontWeight: "bold" }}
-            variant="contained"
-          >
-            Dashboard
-          </Button>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-button",
-            }}
-          >
-            <MenuItem onClick={handleClose}>Profile</MenuItem>
-            <MenuItem onClick={handleClose}>My account</MenuItem>
-            <MenuItem onClick={handleClose}>Logout</MenuItem>
-          </Menu>
+          <Button variant="contained"> Dashboard </Button>
         </Grid>
         <Grid item>
-          <LoginButton/>
+          <LoginButton />
         </Grid>
       </Grid>
     </div>
