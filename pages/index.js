1e3ce76773d65aae77b09d488e9997ecc4dbb314
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-29 16:39:57 -0400

    Update index.js with session

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
+import { useSession } from "next-auth/react";
+
 export default function Home() {
-  return <div style={{marginTop: "2rem"}}>Hello World</div>;
+  const { data: session } = useSession();
+
+  if (!session) {
+    return (
+      <div style={{ marginTop: "2rem" }}>Hello! Welcome to the website.</div>
+    );
+  }
+
+  if (session.user) {
+    return <div style={{ marginTop: "2rem" }}>Hello {session.user.name}</div>;
+  }
 }
