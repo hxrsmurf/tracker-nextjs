6616526bb283ef019e15fc7b023fc6d0ec63e6c7
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-29 15:55:08 -0400

    Initial AccountMenu

--- Components/LoginButton.js
+++ Components/LoginButton.js
@@ -1,5 +1,6 @@
 import { Button } from "@mui/material";
 import { useSession, signIn, signOut } from "next-auth/react";
+import AccountMenu from "./AccountMenu";
 
 export default function LoginButton() {
   const { data: session } = useSession()
@@ -8,6 +9,6 @@ export default function LoginButton() {
     <Button variant="contained" onClick={() => signIn()}>Login</Button>
   )
   return (
-    <Button variant="contained" onClick={() => signOut()}>Logout</Button>
+    <AccountMenu session={session}/>
   )
 }
