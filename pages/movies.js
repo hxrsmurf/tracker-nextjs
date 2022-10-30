92f098a04ab50323168268c3923107006e9736cb
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-29 21:01:38 -0400

    Update movie.js

--- pages/movies.js
+++ pages/movies.js
@@ -1,20 +1,24 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import Form from "../Components/Form";
+import ListMovies from "../Components/ListMovies";
 
-export default function movies() {
+export default function movies(props) {
   const { data: session } = useSession();
+  const [databaseResults, setdatabaseResults] = useState();
   if (!session) useRouter().push("/");
 
-  if (!session.user) return <>Loading...</>
+  if (!session.user) return <>Loading...</>;
 
   return (
     <>
       <div>
-        <Form type="movie" user={session.user.email}/>
+        <Form type="movie" user={session.user.email} />
       </div>
 
       <div>Movies</div>
+      <ListMovies session={session} />
     </>
   );
 }
