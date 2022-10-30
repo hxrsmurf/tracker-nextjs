840d9edd6fedd5c63cb313e1994b07c3a7fa5578
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-30 17:57:59 -0400

    Format footer

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,39 +1,53 @@
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import GitHubIcon from "@mui/icons-material/GitHub";
+
 import { Grid } from "@mui/material";
 import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div
-      style={{
-        position: "absolute",
-        bottom: 20,
-        width: "50%",
-        textAlign: "center",
-      }}
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      style={{ marginTop: "2rem" }}
     >
       <hr style={{ width: "25%" }}></hr>
       <Grid
-        style={{ marginTop: 0, marginBottom: "1rem", textAlign: "center" }}
-        justifyContent="center"
         container
-        spacing={2}
+        justifyContent="center"
+        alignItems="center"
+        style={{ marginTop: ".5rem" }}
       >
         <Grid item>
           <YouTubeIcon />
         </Grid>
         <Grid item>
           <TwitterIcon />
-        </Grid>{" "}
+        </Grid>
         <Grid item>
           <GitHubIcon />
         </Grid>
       </Grid>
-      <Grid>
-        <Grid item><Link href="/privacy">Privacy Policy</Link></Grid>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        style={{ marginTop: ".5rem" }}
+      >
+        <Grid item>
+          <Link href="/privacy">Privacy Policy</Link>
+        </Grid>
       </Grid>
-    </div>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        style={{ marginTop: ".5rem" }}
+      >
+        <Grid item>Copyright © 2022 hxrsmurf</Grid>
+      </Grid>
+    </Grid>
   );
 }
