a01fd7db58044aa4da230c6d7b51910fc5d6f736
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-30 15:38:05 -0400

    initial menu items

--- Components/Navigation.js
+++ Components/Navigation.js
@@ -1,12 +1,14 @@
 import { Grid } from "@mui/material";
 import { useSession } from "next-auth/react";
 import LoginButton from "./LoginButton";
+import MenuItems from "./MenuItems";
 
 export default function Navigation() {
   const { data: session } = useSession();
   return (
     <>
       <Grid>
+      {!session ? <></> : <MenuItems session={session}/>}
         <Grid item>
           <LoginButton session={session} />
         </Grid>
