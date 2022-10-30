3549130fa05232d672dd60bf48380a82a602f8ba
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-30 04:32:13 -0400

    Profile with useSession

--- pages/profile.js
+++ pages/profile.js
@@ -1,9 +1,18 @@
+import { useSession } from "next-auth/react";
+
 export default function profile() {
+  const { data: session } = useSession();
+  if (!session) {
+    console.log("No session");
+    return <>Loading...</>;
+  } else if (!session.user) {
+    console.log("no session user");
+    return <>Loading...</>;
+  }
+
   return (
     <>
-        <div style={{marginTop: "2rem"}}>
-            Profile Page for user.
-        </div>
+      <div style={{ marginTop: "2rem" }}>Profile Page for user.</div>
     </>
-  )
+  );
 }
