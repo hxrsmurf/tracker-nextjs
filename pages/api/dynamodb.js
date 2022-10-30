8132ae7311a3421fac7e03be63495e36b6488d0f
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-29 20:27:34 -0400

    Add putItem dynamodb

--- pages/api/dynamodb.js
+++ pages/api/dynamodb.js
@@ -1,7 +1,7 @@
 import { DynamoDB } from "@aws-sdk/client-DynamoDB";
 import { unstable_getServerSession } from "next-auth/next";
 import { authOptions } from "./auth/[...nextauth]";
-
+import { stringify, v4 as uuidv4 } from "uuid";
 const client = new DynamoDB({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY,
@@ -19,11 +19,38 @@ export default async function handler(req, res) {
     });
   }
 
-  if (req.method === "PUT"){
-    const user = req.query.user
-    const data = req.query.data
-    console.log(data)
-    return res.status(200)
+  if (req.method === "PUT") {
+    const user = req.query.user;
+    const data = req.query.data;
+    const type = req.query.type;
+    const unique_id = uuidv4();
+
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB.html#updateItem-property
+    client.putItem(
+      {
+        TableName: process.env.TABLE_NAME,
+        Item: {
+          user: {
+            S: user,
+          },
+          id: {
+            S: unique_id,
+          },
+          type: {
+            S: type,
+          },
+          data: {
+            S: data,
+          },
+        },
+      },
+      function (err, data) {
+        if (err) console.log(err, err.stack);
+        else return res.status(200).json({ status: data });
+      }
+    );
+
+    return res.status(200).json({ status: "success" });
   }
 
   if (req.method === "GET") {
