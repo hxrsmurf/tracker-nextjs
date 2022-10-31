16179a7c8eee3ada968f34c68648f31be94fc9fe
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-30 20:10:46 -0400

    Re-Add Dashboard Button (#19)

--- Components/Navigation.js
+++ Components/Navigation.js
@@ -1,5 +1,7 @@
+import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 import AccountMenu from "./AccountMenu";
 import LoginButton from "./LoginButton";
@@ -7,11 +9,16 @@ import MenuItems from "./MenuItems";
 
 export default function Navigation() {
   const { data: session } = useSession();
+  const router = useRouter()
+
   return (
     <>
       <Grid container spacing={2}>
+      <Grid item>
+        <Button variant="contained" onClick={() => router.push('/')}>Dashboard</Button>
+      </Grid>
       {!session ? <></> : <MenuItems session={session}/>}
-        <Grid item>
+        <Grid item style={{marginLeft: "1rem"}}>
           <LoginButton session={session} />
         </Grid>
         {!session ? <></> :
