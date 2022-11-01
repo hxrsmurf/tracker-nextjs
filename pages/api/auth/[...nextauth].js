ef7ec94b2623bfed9cc3cc020ab551dad0b1e02e
Kevin Murphy <kevin@hxrsmurf.info>
2022-11-01 18:40:05 -0400

    Add NextJS Auth DynamoDB (Store Info in DB) (#26)

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,26 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import SpotifyProvider from "next-auth/providers/spotify";
 
+import { DynamoDB } from "@aws-sdk/client-dynamodb"
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb"
+import { DynamoDBAdapter } from "@next-auth/dynamodb-adapter"
+
+const DynamoDBClientConfig = {
+  credentials: {
+    accessKeyId: process.env.NEXT_AUTH_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.NEXT_AUTH_AWS_SECRET_KEY
+  },
+  region: 'us-east-1'
+}
+
+const client = DynamoDBDocument.from(new DynamoDB(DynamoDBClientConfig), {
+  marshallOptions: {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+  },
+})
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -15,6 +35,10 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  adapter: DynamoDBAdapter(
+    client,
+    {tableName: process.env.NEXT_AUTH_AWS_TABLE}
+  ),
 };
 
 export default NextAuth(authOptions);
