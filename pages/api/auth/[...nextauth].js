1176a8cf75b7136686b1441ccf44d80740229c59
Kevin Murphy <kevin@hxrsmurf.info>
2022-11-01 18:08:43 -0400

    Add SpotifyProvider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import SpotifyProvider from "next-auth/providers/spotify";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -7,6 +8,10 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_ID,
+      clientSecret: process.env.SPOTIFY_SECRET,
     }),
     // ...add more providers here
   ],
