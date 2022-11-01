b0130bf0789141788afaeae9f41ddf2e855018c7
Kevin Murphy <kevin@hxrsmurf.info>
2022-11-01 19:54:27 -0400

    Add AuthDB.js to query

--- pages/api/db/AuthDb.js
+++ pages/api/db/AuthDb.js
@@ -1,8 +1,17 @@
 import { unstable_getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]";
 
-export default async function handler(req, res) {
+import { DynamoDB, QueryCommand } from "@aws-sdk/client-dynamodb";
 
+const client = new DynamoDB({
+  credentials: {
+    accessKeyId: process.env.NEXT_AUTH_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.NEXT_AUTH_AWS_SECRET_KEY,
+  },
+  region: process.env.REGION,
+});
+
+export default async function handler(req, res) {
   const session = await unstable_getServerSession(req, res, authOptions);
 
   if (!session) {
@@ -10,11 +19,21 @@ export default async function handler(req, res) {
       content: "Not authorized",
     });
   }
+
   if (req.method === "GET") {
     const user_id = req.query.id
-
-    return res.send({
-      message: user_id
-    })
-  }
+      const params = {
+        TableName: process.env.NEXT_AUTH_AWS_TABLE,
+        ScanIndexForward: false,
+        KeyConditionExpression: "pk = :value",
+        ExpressionAttributeValues: {
+          ":value": {
+            S: 'USER#' + user_id,
+          },
+        },
+      };
+      const command = new QueryCommand(params);
+      const data = await client.send(command);
+      return res.send({accessToken: data.Items[4]['access_token']});
+    }
 }
