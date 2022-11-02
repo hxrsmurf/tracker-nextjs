cf4870301b4243a322ad008ab03cd14beaa01034
Kevin Murphy <kevin@hxrsmurf.info>
2022-11-02 17:54:44 -0400

    Update spotify index

--- pages/spotify/index.js
+++ pages/spotify/index.js
@@ -1,38 +1,17 @@
 import { useSession } from "next-auth/react"
-import { useEffect, useState } from "react"
-
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
+import RecentlyPlayed from '../../Components/Spotify/RecentlyPlayed'
 
 export default function index() {
     const { data: session } = useSession()
-    const [helloData, setHelloData] = useState()
-
-    useEffect(() => {
-        const hello = async () => {
-            const req = await fetch('/api/hello')
-            const res = await req.json()
-            setHelloData(res)
-        }
-        hello()
-
-    }, [])
 
     if (!session) return <>Loading session...</>
     if (!session.user) return <>Loading user...</>
-    if (!helloData) return <>Loading Hello Data...</>
 
     return (
         <>
             <div className="content">
-                {session.user.email} {helloData.message}
-                <Accordion style={{ marginTop: "2rem" }}>
-                    <AccordionSummary>Recently Played</AccordionSummary>
-                    <AccordionDetails>
-                        Hello
-                    </AccordionDetails>
-                </Accordion>
+                {session.user.email}
+                <RecentlyPlayed/>
             </div>
         </>
     )
