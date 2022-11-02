9d5032cea849685b5f051d0511502457924c1fb0
Kevin Murphy <kevin@hxrsmurf.info>
2022-11-02 18:27:47 -0400

    Add router push / if not logged in

--- pages/spotify/index.js
+++ pages/spotify/index.js
@@ -1,12 +1,17 @@
 import { useSession } from "next-auth/react"
+import { useRouter } from "next/router"
 import NowPlaying from "../../Components/Spotify/NowPlaying"
 import RecentlyPlayed from '../../Components/Spotify/RecentlyPlayed'
 
 export default function Index() {
     const { data: session } = useSession()
+    const router = useRouter()
 
-    if (!session) return <>Loading session...</>
-    if (!session.user) return <>Loading user...</>
+    if (!session) {
+        router.push("/");
+        return <></>
+      }
+    if (!session.user) return <></>
 
     return (
         <>
