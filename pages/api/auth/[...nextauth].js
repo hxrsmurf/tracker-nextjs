89e5f7a40cfba353e58c99435c6b57d1f63b1fa1
Kevin Murphy <kevin@hxrsmurf.info>
2022-11-02 08:37:53 -0400

    Formatting

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -23,10 +23,10 @@ const client = DynamoDBDocument.from(new DynamoDB(DynamoDBClientConfig), {
   },
 })
 
-async function updateDBAccessToken(id, access_token, expires_at, sort_key, partition_key){
+async function updateDBAccessToken(id, access_token, expires_at, sort_key, partition_key) {
   const params = {
     TableName: process.env.NEXT_AUTH_AWS_TABLE,
-    Key: {'pk': partition_key, 'sk': sort_key},
+    Key: { 'pk': partition_key, 'sk': sort_key },
     UpdateExpression: 'SET access_token = :access, expires_at = :expires',
     ExpressionAttributeValues: {
       ':access': {
@@ -62,37 +62,39 @@ async function generateDBParameter(id) {
   var sort_key = null
   var partition_key = null
 
-  {data.Items.map((d, id)=> {
-    {d.refresh_token ? refresh_token = d.refresh_token.S: null}
-    {d.access_token ? access_token = d.access_token.S: null}
-    {d.expires_at ? expires_at = d.expires_at.N: null}
-    {d.sk ? sort_key = d.sk: null}
-    {d.pk ? partition_key = d.pk: null}
-  })}
+  {
+    data.Items.map((d, id) => {
+      { d.refresh_token ? refresh_token = d.refresh_token.S : null }
+      { d.access_token ? access_token = d.access_token.S : null }
+      { d.expires_at ? expires_at = d.expires_at.N : null }
+      { d.sk ? sort_key = d.sk : null }
+      { d.pk ? partition_key = d.pk : null }
+    })
+  }
 
-  const current_time = Date.now()/1000
+  const current_time = Date.now() / 1000
 
   // If access token expired, grab a new one
-  if (current_time > expires_at){
+  if (current_time > expires_at) {
     console.log('Access Token Expired!')
 
-  const url = 'https://accounts.spotify.com/api/token?' + new URLSearchParams({
-    client_id: process.env.SPOTIFY_ID,
-    client_secret: process.env.SPOTIFY_SECRET,
-    grant_type: 'refresh_token',
-    refresh_token: refresh_token
-  })
+    const url = 'https://accounts.spotify.com/api/token?' + new URLSearchParams({
+      client_id: process.env.SPOTIFY_ID,
+      client_secret: process.env.SPOTIFY_SECRET,
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    })
 
-  const refresh_response = await fetch(url, {method: 'POST', headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
-  const result = await refresh_response.json()
-  access_token = result.access_token
-  expires_at = result.expires_in + current_time
+    const refresh_response = await fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
+    const result = await refresh_response.json()
+    access_token = result.access_token
+    expires_at = result.expires_in + current_time
 
-  await updateDBAccessToken(id, access_token, expires_at, sort_key, partition_key)
+    await updateDBAccessToken(id, access_token, expires_at, sort_key, partition_key)
 
   }
 
-  return {refresh_token, access_token, expires_at}
+  return { refresh_token, access_token, expires_at }
 
 }
 
