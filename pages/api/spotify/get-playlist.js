81b1c846b2bfbed7116abcc75d13eda013eda0b8
Kevin Murphy <kevin@hxrsmurf.info>
2022-11-03 07:56:00 -0400

    Update get-playlist

--- pages/api/spotify/get-playlist.js
+++ pages/api/spotify/get-playlist.js
@@ -7,45 +7,10 @@ var SpotifyWebApi = require('spotify-web-api-node');
 // https://developer.spotify.com/documentation/web-api/reference/#/operations/get-the-users-currently-playing-track
 
 export default async function handler(req, res) {
-    const session = await unstable_getServerSession(req, res, authOptions)
-    const api_key = req.query.key
-    const email = req.query.email
-
-    if (!session || !api_key || !email) {
-        if (api_key && email) {
-            console.log('Granting access, has API key')
-        } else if (session) {
-            console.log('Has session, granting access')
-        }
-        else {
-            return res.status(401).send({
-                'message': 'Not authorized because no authentication.'
-            })
-        }
-    }
-
-    var access_token = null
-
-    if (session) {
-        access_token = session.tokens.access_token
-    } else {
-        // Two options for this:
-        // 1 - query NextJS Auth's database for the email, then the primary key to get access_token
-        // 2 - Store access_token in my other user table which also has categories.
-        access_token = null
-    }
-
-    if (!access_token) {
-        return res.status(400).send({
-            message: 'Error getting now playing.'
-        })
-    }
-    var spotifyApi = new SpotifyWebApi({
-        accessToken: access_token
-    })
-
     /// This does not work if not a playlist. I should figure out how to create another handler function to get the context
     // Then use that context to get the information
+    // We could assume an API key or something.
+    // I'm thinking a central authentication hook and then go from there.
 
     spotifyApi.getMyCurrentPlayingTrack({
     }).then(function (data) {
