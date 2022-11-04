3579f9028230d34170bbdaea5c1b71ed29b91ddb
Kevin Murphy <kevin@hxrsmurf.info>
2022-11-03 23:17:37 -0400

    Update settings to use a button to refresh share

--- Components/Spotify/Settings.js
+++ Components/Spotify/Settings.js
@@ -3,31 +3,51 @@ import { useSession } from 'next-auth/react';
 import React, { useEffect, useState } from 'react'
 
 export default function SpotifySettings() {
- const { data: session } = useSession();
- const [sharing, setSharing] = useState()
- const [shareId, setShareId] = useState(null)
+  const { data: session } = useSession();
+  const [sharing, setSharing] = useState()
+  const [shareId, setShareId] = useState()
+  const [hideButton, setHideButton] = useState(true) // Helps with preventing false loads. Not sure how to fix that as sometimes it doesn't refresh
 
- const handleShareClick = () => {
-    setSharing(sharing ? false: true)
+  // Get current setting
+  useEffect(() => {
+    {
+      const query = async () => {
+        const req = await fetch('http://localhost:3000/api/db/Spotify?now_playing_public')
+        const res = await req.json()
+        setShareId(res.setting.S)
+      }
+      query()
+      setHideButton(false)
+    }
+  }, [hideButton])
+
+  const handleShareClick = () => {
+    setSharing(shareId === 'disabled' ? 'enabled' : 'disabled')
+    setHideButton(true)
 
     const submitData = async () => {
-        const query = await fetch('http://localhost:3000/api/db/Spotify', {method: 'PUT', body: Boolean(sharing)})
-        const response = await query.json()
-        setShareId(response)
+      const query = await fetch('http://localhost:3000/api/db/Spotify', { method: 'PUT', body: sharing })
+      const response = await query.json()
+      setShareId(response.share_id)
     }
-    console.log(shareId)
     submitData()
- }
+    setHideButton(true)
+  }
 
   return (
     <Accordion>
-        <AccordionSummary>NOW PLAYING</AccordionSummary>
-        <AccordionDetails>
-            <Grid>
-            {!shareId ? <></> : <><Grid item>{shareId.share_id}</Grid></>}
-            <Grid item><Button variant='contained' onClick={()=> handleShareClick()}>{sharing ? <>Disable</> : <>Enable</>} Public Sharing</Button></Grid>
-            </Grid>
-        </AccordionDetails>
+      <AccordionSummary>NOW PLAYING</AccordionSummary>
+      <AccordionDetails>
+        <Grid>
+          <Grid item>{shareId === 'disabled' ? <></> : <>{shareId}</>}</Grid>
+          <Grid item>{hideButton ? <></> :
+            <>
+              <Button variant='contained' onClick={() => handleShareClick()}>{shareId === 'disabled' ? <>Enable</> : <>Disable</>} Public Sharing</Button>
+            </>
+          }
+          </Grid>
+        </Grid>
+      </AccordionDetails>
     </Accordion>
   )
 }
--- pages/api/db/Spotify.js
+++ pages/api/db/Spotify.js
@@ -2,7 +2,7 @@ import { unstable_getServerSession } from "next-auth/next";
 import { authOptions } from "../auth/[...nextauth]";
 import { stringify, v4 as uuidv4 } from "uuid"
 
-import { DynamoDB, PutItemCommand } from "@aws-sdk/client-dynamodb"
+import { DynamoDB, PutItemCommand, QueryCommand } from "@aws-sdk/client-dynamodb"
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb"
 
 const DynamoDBClientConfig = {
@@ -25,15 +25,45 @@ const client = DynamoDBDocument.from(new DynamoDB(DynamoDBClientConfig), {
 export default async function handler(req, res) {
     const session = await unstable_getServerSession(req, res, authOptions);
 
+    if (!session) {
+      res.status(401).json({ message: "You must be logged in." });
+      return;
+    }
+
+    if (req.method == 'GET') {
+        const params = {
+          TableName: process.env.TABLE_USER,
+          KeyConditionExpression: "email = :value",
+          ExpressionAttributeNames: {'#t': 'type'},
+          FilterExpression: '#t = :setting',
+          ExpressionAttributeValues: {
+            ":value": {
+              S: session.user.email,
+            },
+            ":setting": {
+              S: 'setting'
+            },
+          },
+        };
+
+        const command = new QueryCommand(params);
+        const data = await client.send(command);
+        const current_setting = data.Items[0].data
+
+        res.send({setting: current_setting})
+    }
+
     if (req.method == 'PUT') {
         var uuid = null
 
         // We actually send a string
-        if (req.body === 'true') {
+        if (req.body === 'enabled') {
             uuid = uuidv4().replace(/-/g, "").slice(0, 10)
         } else {
-            uuid = 'false'
+            uuid = 'disabled'
         }
+
+        console.log(uuid)
 
         const params = {
             TableName: process.env.TABLE_USER,
